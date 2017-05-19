c7e8e255a471bef381a24d6433b2de997ae6b8bb
vagrant <vagrant@homestead>
2017-05-19 04:40:15 +0000

    config UT

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -3,8 +3,32 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use DB;
+use App\Eloquent\User;
 
 abstract class TestCase extends BaseTestCase
 {
     use CreatesApplication;
+
+    public function setUp()
+    {
+        parent::setUp();
+        $this->artisan('db:seed');
+    }
+
+    public function getHeaders($header = [])
+    {
+        $default = [
+            'Accept' => 'application/json',
+        ];
+
+        $headers = count($header) ? array_merge($default, $header) : $default;
+
+        return $this->transformHeadersToServerVars($headers);
+    }
+
+    public function createUser()
+    {
+        return factory(User::class)->create();
+    }
 }
