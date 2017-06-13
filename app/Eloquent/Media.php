cd60be180b7d45141a89a8581a7148bfb21dfcf1
hoquochai <hoquochaibkdn@gmail.com>
2017-06-13 17:06:13 +0700

    Task #122214: Render images for multi platform

--- app/Eloquent/Media.php
+++ app/Eloquent/Media.php
@@ -21,7 +21,9 @@ class Media extends Model
         'target_id',
     ];
 
-    protected $hidden = ['target_id', 'target_type'];
+    protected $hidden = ['target_id', 'target_type', 'thumb_path'];
+
+    protected $appends = ['mobile', 'web'];
 
     public function target()
     {
@@ -30,22 +32,48 @@ class Media extends Model
 
     private function responseMediaStorage($size = null)
     {
+        if (is_array($size)) {
+            $mediaPath = [];
+
+            foreach ($size as $item => $value) {
+                $mediaPath[$item] = route('image',
+                    ['path' => app()['glide.builder']->getUrl($this->path, ['p' => ($value) ?: null])]
+                );
+            }
+
+            return $mediaPath;
+        }
+
         return route('image',
             ['path' => app()['glide.builder']->getUrl($this->path, ['p' => ($size) ?: null])]
         );
     }
 
-    public function getThumbPathAttribute()
+    public function getMobileAttribute()
     {
         if ($this->path) {
-            return $this->responseMediaStorage('thumbnail');
+            return $this->responseMediaStorage([
+                'thumbnail_path' => 'thumbnail',
+                'small_path' => 'small',
+                'medium_path' => 'medium',
+                'large_path' => 'large',
+            ]);
         }
 
         return $this->thumb_path;
     }
 
-    public function getFullPathAttribute()
+    public function getWebAttribute()
     {
-        return $this->responseMediaStorage('medium');
+        if ($this->path) {
+            return $this->responseMediaStorage([
+                'thumbnail_path' => 'thumbnail_web',
+                'small_path' => 'small_web',
+                'medium_path' => 'medium_web',
+                'large_path' => 'large_web',
+            ]);
+        }
+
+        return $this->thumb_path;
     }
 }
